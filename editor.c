8a2b14b5d6b4cffb9d5edb7dd19328d17b9446e8
Justin <jko1@cpp.edu>
2021-12-01 15:10:44 -0800

    completed section 1

--- editor.c
+++ editor.c
@@ -1,38 +1,61 @@
+/*** INCLUDES ***/
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include<termios.h>
 #include <unistd.h>
 
+/*** DATA ***/
 struct termios orig_termios;
- 
+
+/*** TERMINAL ***/
+//allows for error handling
+void die(const char *s) {
+    perror(s);
+    exit(1);
+}
 //turning off rawmode/ echoing when the program exits
 void disableRawMode(){
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+        die("tcsetattr");
 }
 //Turn off echoing, meaning you enable raw mode
 void enableRawMode(){
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr"));
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
-    //ICANON flag turns off canonical mode
-    raw.c_lflag &= ~(ECHO | ICANON);
+    //allows terminate/suspend signals to be read(ctrl-s, ctrl-q) as bytes
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    //fixes newline charactesr
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    //ICANON flag turns off canonical mode, ISIG turns off terminate/suspend signals
+    //IEXTEN disables ctrl-v and they now can be read as bytes
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN |ISIG);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcgetattr");
 }
+
+/*** INIT ***/
 int main() {
     enableRawMode();
-    char c;
-    //reads 1 byte from standard input into char c, as long it is not 'q'
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+    while(1){
+        char c '\0'
+        if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
         //iscntrl() checks if a character is a control character
         if (iscntrl(c)) {
-            printf("%d\n", c);
+            printf("%d\r\n", c);
         }
         else {
-            printf("%d ('%c')\n", c, c);
+            printf("%d ('%c')\r\n", c, c);
         }
+        if(c == 'q')
+            break;
     }
+    //reads 1 byte from standard input into char c, as long it is not 'q'
     return 0;
 }
