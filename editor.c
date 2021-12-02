a1f9d6b3c044a074634b4bceacffecdcba60a017
Justin <jko1@cpp.edu>
2021-12-02 15:08:13 -0800

    compile error

--- editor.c
+++ editor.c
@@ -198,7 +198,7 @@ void editorDrawRows(struct abuf *ab) {
                 abAppend(ab, "~", 1);
                 padding--;
             }
-            while (padding-) abAppend(ab, " " , 1);
+            while (padding--) abAppend(ab, " " , 1);
             abAppend(ab, welcome, welcomelen);
         }
         else{
@@ -226,7 +226,7 @@ void editorRefreshScreen() {
     snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
     abAppend(&ab, buf, strlen(buf));
 
-    apAppend(&ab, "\x1b[?25h", 6);
+    abAppend(&ab, "\x1b[?25h", 6);
 
     write(STDOUT_FILENO, ab.b, ab.len);
     abFree(&ab);
